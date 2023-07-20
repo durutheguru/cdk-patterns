d49b245b0194ae67cf8732993518506b596e0cb1
Julian Duru <durutheguru@gmail.com>
2023-07-20 20:33:59 +0100

    - ...

--- code-pipeline/src/main/java/com/julianduru/cdk/stages/test/RdsStack.java
+++ code-pipeline/src/main/java/com/julianduru/cdk/stages/test/RdsStack.java
@@ -3,6 +3,7 @@ package com.julianduru.cdk.stages.test;
 /**
  * created by Julian Dumebi Duru on 19/07/2023
  */
+import com.julianduru.cdk.util.JSONUtil;
 import software.amazon.awscdk.CfnOutput;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.services.ec2.*;
@@ -10,15 +11,28 @@ import software.amazon.awscdk.services.rds.*;
 import software.amazon.awscdk.services.secretsmanager.*;
 import software.constructs.Construct;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RdsStack extends Stack {
 
 
     public RdsStack(final Construct scope, final String id, final Vpc vpc) {
         super(scope, id);
 
+        Map<String, String> secretsMap = new HashMap<>();
+        secretsMap.put("username", "postgres");
+
         // Create RDS Database Secret
-        ISecret secret = Secret.Builder.create(this, "DatabaseSecret")
-            .secretName("mydatabase/credentials")
+        // Templated secret with username and password fields
+        Secret secret = Secret.Builder.create(this, "TemplatedSecret")
+            .generateSecretString(
+                SecretStringGenerator.builder()
+                    .secretStringTemplate(JSONUtil.asJsonString(secretsMap, ""))
+                    .generateStringKey("password")
+                    .excludeCharacters("/@\"")
+                    .build()
+            )
             .build();
 
         // Create RDS Database Instance
@@ -30,7 +44,7 @@ public class RdsStack extends Stack {
                         .build()
                 )
             )
-            .instanceIdentifier("mydatabase")
+            .instanceIdentifier("my_rds_database")
             .credentials(Credentials.fromSecret(secret))
             .vpc(vpc)
             .vpcSubnets(
