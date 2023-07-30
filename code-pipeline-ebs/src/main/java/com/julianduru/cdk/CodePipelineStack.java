32a9b9ab44a1773926d4b7f28c800b1e5bf29d32
Julian Duru <durutheguru@gmail.com>
2023-07-30 02:13:51 +0100

    - ...

--- code-pipeline-ebs/src/main/java/com/julianduru/cdk/CodePipelineStack.java
+++ code-pipeline-ebs/src/main/java/com/julianduru/cdk/CodePipelineStack.java
@@ -1,20 +1,21 @@
 package com.julianduru.cdk;
 
-import com.julianduru.cdk.stages.TestStage;
-import software.amazon.awscdk.Environment;
+import com.julianduru.cdk.stages.test.EbsStack;
+import software.amazon.awscdk.SecretValue;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.StageProps;
-import software.amazon.awscdk.pipelines.CodePipeline;
-import software.amazon.awscdk.pipelines.CodePipelineSource;
-import software.amazon.awscdk.pipelines.ShellStep;
-import software.amazon.awscdk.services.iam.*;
+import software.amazon.awscdk.services.codebuild.*;
+import software.amazon.awscdk.services.codepipeline.Artifact;
+import software.amazon.awscdk.services.codepipeline.Pipeline;
+import software.amazon.awscdk.services.codepipeline.PipelineProps;
+import software.amazon.awscdk.services.codepipeline.StageProps;
+import software.amazon.awscdk.services.codepipeline.actions.CodeBuildAction;
+import software.amazon.awscdk.services.codepipeline.actions.ElasticBeanstalkDeployAction;
+import software.amazon.awscdk.services.codepipeline.actions.GitHubSourceAction;
 import software.constructs.Construct;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 public class CodePipelineStack extends Stack {
 
@@ -27,36 +28,142 @@ public class CodePipelineStack extends Stack {
     public CodePipelineStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        CodePipeline pipeline = CodePipeline.Builder.create(this, "pipeline")
-            .pipelineName("Pipeline")
-            .synth(
-                ShellStep.Builder.create("Synth")
-                    .input(CodePipelineSource.gitHub("durutheguru/cdk-patterns", "main"))
-                    .commands(
-                        Arrays.asList(
-                            "npm install -g aws-cdk",
-                            "cd code-pipeline-ebs",
-                            "cdk deploy --all --verbose --require-approval never"
-                        )
-                    )
-                    .primaryOutputDirectory("code-pipeline-ebs/cdk.out")
-                    .build()
+//        CodePipeline pipeline = CodePipeline.Builder.create(this, "pipeline")
+//            .pipelineName("Pipeline")
+//            .synth(
+//                ShellStep.Builder.create("Synth")
+//                    .input(CodePipelineSource.gitHub("durutheguru/cdk-patterns", "main"))
+//                    .commands(
+//                        Arrays.asList(
+//                            "npm install -g aws-cdk",
+//                            "cd code-pipeline-ebs",
+//                            "cdk deploy --all --verbose --require-approval never"
+//                        )
+//                    )
+//                    .primaryOutputDirectory("code-pipeline-ebs/cdk.out")
+//                    .build()
+//            )
+//            .build();
+
+
+//        pipeline.addStage(
+//            new TestStage(
+//                this, "testStageId",
+//                StageProps.builder()
+//                    .env(
+//                        Environment.builder()
+//                            .account("058486276453")
+//                            .region("us-east-1")
+//                            .build()
+//                    )
+//                    .build()
+//            )
+//        );
+
+
+        // GitHub source repository information
+        String githubOwner = "durutheguru";
+        String githubRepo = "cdk-patterns";
+        String githubBranch = "main";
+
+
+        // Define the CodeBuild project
+//        Project buildProject = new Project(this, "MyProject", ProjectProps.builder()
+//            .source(
+//                Source.gitHub(
+//                    GitHubSourceProps.builder()
+//                        .owner(githubOwner)
+//                        .repo(githubRepo)
+//                        .branchOrRef(githubBranch)
+//                        .reportBuildStatus(true)
+//                        .webhook(true)
+//                        .build()
+//                )
+//            )
+//            .environment(
+//                BuildEnvironment.builder().buildImage(LinuxBuildImage.STANDARD_5_0).build()
+//            )
+//            .build());
+
+        Project buildProject = Project.Builder.create(this, "EBS-Project")
+            .source(
+                Source.gitHub(
+                    GitHubSourceProps.builder()
+                        .owner(githubOwner)
+                        .repo(githubRepo)
+                        .branchOrRef(githubBranch)
+                        .reportBuildStatus(true)
+                        .webhook(true)
+                        .build()
+                )
+            )
+            .buildSpec(
+                BuildSpec.fromSourceFilename("buildspec.yml")
+            )
+            .environment(
+                BuildEnvironment.builder().buildImage(LinuxBuildImage.STANDARD_5_0).build()
             )
             .build();
 
-        pipeline.addStage(
-            new TestStage(
-                this, "testStageId",
-                StageProps.builder()
-                    .env(
-                        Environment.builder()
-                            .account("058486276453")
-                            .region("us-east-1")
-                            .build()
-                    )
-                    .build()
+
+
+        Pipeline pipeline = new Pipeline(this, "EBS-CodePipeline", PipelineProps.builder()
+            .stages(
+                Arrays.asList(
+                    StageProps.builder()
+                        .stageName("Source")
+                        .actions(
+                            Collections.singletonList(
+                                GitHubSourceAction.Builder.create()
+                                    .actionName("GitHubSourceAction")
+                                    .owner(githubOwner)
+                                    .repo(githubRepo)
+                                    .branch(githubBranch)
+                                    .oauthToken(SecretValue.secretsManager("github-token"))
+                                    .output(Artifact.artifact("EBS-SourceArtifact"))
+                                    .build()
+                            )
+                        )
+                        .build(),
+
+                    StageProps.builder()
+                        .stageName("Build")
+                        .actions(
+                            Collections.singletonList(
+                                CodeBuildAction.Builder.create()
+                                    .actionName("CodeBuild")
+                                    .project(buildProject)
+                                    .input(Artifact.artifact("EBS-SourceArtifact"))
+                                    .outputs(
+                                        Collections.singletonList(
+                                            Artifact.artifact("EBS-BuildArtifact")
+                                        )
+                                    )
+                                    .build()
+                            )
+                        )
+                        .build(),
+
+                    StageProps.builder()
+                        .stageName("Deploy")
+                        .actions(Collections.singletonList(getEBSDeployAction()))
+                        .build()
+                )
             )
-        );
+            .build());
+
+    }
+
+
+    private ElasticBeanstalkDeployAction getEBSDeployAction() {
+        EbsStack ebsStack = new EbsStack(this, "ebsStackId");
+
+        return ElasticBeanstalkDeployAction.Builder.create()
+            .actionName("ElasticBeanstalkDeployAction")
+            .applicationName(ebsStack.getApplication().getApplicationName())
+            .environmentName(ebsStack.getEbsEnvironment().getEnvironmentName())
+            .input(Artifact.artifact("EBS-BuildArtifact"))
+            .build();
     }
 
 
--- code-pipeline-ebs/src/main/java/com/julianduru/cdk/stages/test/EbsStack.java
+++ code-pipeline-ebs/src/main/java/com/julianduru/cdk/stages/test/EbsStack.java
@@ -1,12 +1,18 @@
 package com.julianduru.cdk.stages.test;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.elasticbeanstalk.CfnApplication;
+import software.amazon.awscdk.services.elasticbeanstalk.CfnApplicationVersion;
 import software.amazon.awscdk.services.elasticbeanstalk.CfnEnvironment;
 import software.amazon.awscdk.services.iam.CfnInstanceProfile;
 import software.amazon.awscdk.services.iam.ManagedPolicy;
 import software.amazon.awscdk.services.iam.Role;
 import software.amazon.awscdk.services.iam.ServicePrincipal;
+import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.s3.BucketProps;
 import software.constructs.Construct;
 
 import java.util.Arrays;
@@ -18,22 +24,82 @@ import java.util.List;
  */
 public class EbsStack extends Stack {
 
+    private final CfnApplication application;
+
+
+    private final CfnEnvironment environment;
+
+
     public EbsStack(final Construct scope, final String id) {
-        super(scope, id);
+        this(scope, id, null);
+    }
 
 
+    public EbsStack(@Nullable Construct scope, @Nullable String id, @Nullable StackProps props) {
+        super(scope, id, props);
+
+        String appName = "TestEBSApplication";
+
+        application = createApplication(appName);
+
+
+        // Define your S3 bucket
+        Bucket bucket = new Bucket(this, appName + "EBS-Resource-Bucket", BucketProps.builder()
+            .bucketName(appName + "Bucket")
+            .build());
+
+
+        CfnInstanceProfile instanceProfile = createEc2InstanceProfile(appName);
+
+        List<CfnEnvironment.OptionSettingProperty> applicationSettings = Arrays.asList(
+            CfnEnvironment.OptionSettingProperty.builder()
+                .namespace("aws:autoscaling:launchconfiguration")
+                .optionName("InstanceType")
+                .value("t2.micro")
+                .build(),
+
+            CfnEnvironment.OptionSettingProperty.builder()
+                .namespace("aws:autoscaling:launchconfiguration")
+                .optionName("IamInstanceProfile")
+                .value(instanceProfile.getInstanceProfileName())
+                .build()
+        );
+
+
+        CfnApplicationVersion applicationVersion = CfnApplicationVersion.Builder.create(this, "TestEBSApplicationVersion")
+            .applicationName(application.getApplicationName())
+            .sourceBundle(
+                CfnApplicationVersion.SourceBundleProperty.builder()
+                    .s3Bucket("julianduru-cdk")
+                    .s3Key("test-eb-app.zip")
+                    .build()
+            )
+            .build();
+
+
+        environment = CfnEnvironment.Builder.create(this, "TestEBSEnvironment")
+            .applicationName(application.getApplicationName())
+            .environmentName("TestEBSEnvironment")
+            .solutionStackName("64bit Amazon Linux 2 v3.4.9 running Corretto 17")
+            .optionSettings(applicationSettings)
+//            .ver
+            .build();
+
+        environment.addDependency(application);
+    }
+
+
+    private CfnApplication createApplication(String appName) {
         Role serviceRole = Role.Builder.create(this, "EBSServiceRole")
             .assumedBy(new ServicePrincipal("elasticbeanstalk.amazonaws.com"))
             .managedPolicies(
-                Arrays.asList(
-                    ManagedPolicy.fromAwsManagedPolicyName("AWSElasticBeanstalkFullAccess")
+                Collections.singletonList(
+                    ManagedPolicy.fromAwsManagedPolicyName("AdministratorAccess-AWSElasticBeanstalk")
                 )
             )
             .build();
 
-        String appName = "TestEBSApplication";
-
-        CfnApplication application = CfnApplication.Builder.create(this, "TestEBSApplication")
+        return CfnApplication.Builder.create(this, "TestEBSApplication")
             .applicationName(appName)
             .resourceLifecycleConfig(
                 CfnApplication.ApplicationResourceLifecycleConfigProperty.builder()
@@ -52,38 +118,29 @@ public class EbsStack extends Stack {
                     .build()
             )
             .build();
+    }
 
 
+    private CfnInstanceProfile createEc2InstanceProfile(String appName) {
         Role ec2Role = Role.Builder.create(this, appName + "-aws-elasticbeanstalk-ec2-role`")
             .assumedBy(new ServicePrincipal("ec2.amazonaws.com"))
             .build();
         ec2Role.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName("AWSElasticBeanstalkWebTier"));
 
-        CfnInstanceProfile instanceProfile = CfnInstanceProfile.Builder.create(this, appName + "-InstanceProfile")
+        return CfnInstanceProfile.Builder.create(this, appName + "-InstanceProfile")
             .roles(Collections.singletonList(ec2Role.getRoleName()))
             .instanceProfileName(appName + "-InstanceProfile")
             .build();
+    }
 
-        List<CfnEnvironment.OptionSettingProperty> applicationSettings = Arrays.asList(
-            CfnEnvironment.OptionSettingProperty.builder()
-                .namespace("aws:autoscaling:launchconfiguration")
-                .optionName("InstanceType")
-                .value("t2.micro")
-                .build(),
 
-            CfnEnvironment.OptionSettingProperty.builder()
-                .namespace("aws:autoscaling:launchconfiguration")
-                .optionName("IamInstanceProfile")
-                .value(instanceProfile.getInstanceProfileName())
-                .build()
-        );
+    public CfnApplication getApplication() {
+        return application;
+    }
 
-        CfnEnvironment environment = CfnEnvironment.Builder.create(this, "TestEBSEnvironment")
-            .applicationName(application.getApplicationName())
-            .environmentName("TestEBSEnvironment")
-            .solutionStackName("64bit Amazon Linux 2 v3.4.9 running Corretto 17")
-            .optionSettings(applicationSettings)
-            .build();
+
+    public CfnEnvironment getEbsEnvironment() {
+        return environment;
     }
 
 
